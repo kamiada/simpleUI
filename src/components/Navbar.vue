0ba0c40e15d81415e91e5104851b13cf1b0bf7d2
kamiada <kamiada528@gmail.com>
2021-03-03 22:41:33 +0000

    okay, getting there

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar">
+    <ul>
+      <li>
+        <span>Menu</span>
+      </li>
+      <li class="nav-item">
+        <button>First Item</button>
+      </li>
+      <li class="nav-item">
+        <button>Second Item</button>
+      </li>
+      <li class="nav-item">
+        <button>Third Item</button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import "./style.scss";
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped></style>
