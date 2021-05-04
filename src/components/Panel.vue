4f0e1e3542994842f68ab676244fbd851ebb83bc
kamiada <kamiada528@gmail.com>
2021-05-04 09:56:43 +0100

    set up

---
+++ src/components/Panel.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="panel_holder"></div>
+</template>
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {}
+});
+</script>
---
+++ src/components/Tile.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="tile_holder"></div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
